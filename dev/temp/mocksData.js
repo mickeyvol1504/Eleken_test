97e2fac2f69b3bdb9acedad822f1aaef35b0c7b1
Michael <michael.voloshyn@gmail.com>
2017-03-14 16:42:05 +0200

    Remove dev from .gitignore

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,12 @@
+'head': {
+    defaults: {
+        title: 'Eleken test',
+        useSocialMetaTags: true
+    }
+}
+,
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
